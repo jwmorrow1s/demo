339f7f2c36c29bbf9b8085591f828a4fc330cab3
Jeff Morrow <jwmorrow1s@gmail.com>
2021-02-02 22:41:13 -0600

    cleaned up demo page and added todo

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
-  // console.log(showModal);
-
-  return (<><button onClick={() => {
+  return (<>
+  <div id="parent" style={{ height: '400px', width: '50%', backgroundColor: 'lightslategray', margin: '5% auto'}}>
+    <Modal size='xl'
+            isActive={showModal} 
+            confirmAction={() => setShowModal(false)}
+            closeAction={() => setShowModal(false)} 
+            adjustToParent={false}
+            ></Modal>
+  </div>
+  {/* TODO: create form for changing sizes of modal as well as placing it within div */}
+  <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+  <button 
+    onClick={() => {
     setShowModal(!showModal);
   }}>Click Me!</button>
-    <div id="parent" style={{ height: '400px', width: '50%', backgroundColor: 'lightslategray', margin: '200px 0 0 100px'}}>
-      <Modal size='xl'
-             isActive={showModal} 
-             confirmAction={() => setShowModal(false)}
-             closeAction={() => setShowModal(false)} 
-             adjustToParent={false}
-             ></Modal>
-             
-    </div>
+  </div>
   </>);
 }
 export default App;
